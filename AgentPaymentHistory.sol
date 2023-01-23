acae0715d494bf8957c0f32f223ac2dd05c8e116
diamondyhand <masterdevv278@gmail.com>
2023-01-23 02:16:16 -0500

    fix: reduce require string for gas optimization in AgentPaymentHistory.

--- AgentPaymentHistory.sol
+++ AgentPaymentHistory.sol
@@ -4,34 +4,30 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AgentPaymentHistory is Ownable {
-    string private AUTHORIZED_ERROR = "Caller is not authorized";
     address private operator;
 
     modifier onlyAuthorized() {
         require(
             msg.sender == owner() || msg.sender == operator,
-            AUTHORIZED_ERROR
+            "NOT_AUTHORIZED"
         );
         _;
     }
 
     mapping(address => uint256) private transferHistory;
 
-    function updateTransferHistory(address _agent, uint256 _amount)
-        external
-        onlyAuthorized
-        returns (address agent, uint256 amount)
-    {
+    function updateTransferHistory(
+        address _agent,
+        uint256 _amount
+    ) external onlyAuthorized returns (address agent, uint256 amount) {
         transferHistory[_agent] += _amount;
         return (_agent, transferHistory[_agent]);
     }
 
     // this function is used to get history of payed commission to specific account
-    function getTransferHistory(address _wallet)
-        external
-        view
-        returns (uint256)
-    {
+    function getTransferHistory(
+        address _wallet
+    ) external view returns (uint256) {
         require(_wallet != address(0), "Trying to query none existent agent");
         return transferHistory[_wallet];
     }
